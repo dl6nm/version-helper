55c6e5ce6f5699344eb9fc2ca40c686108e651a8
dl6nm <mail@dl6nm.de>
2021-07-28 14:22:01 +0200

    Add initialization method to class Version and change 'core' to be a property

--- version_helper/version.py
+++ version_helper/version.py
@@ -1,28 +1,33 @@
 import re
 
 
+SEMVER_PATTERN = r'(\d+)\.(\d+)\.(\d+)(?:\-((?:[\w\d\-]+\.?)+))?(?:\+((?:[\w\d\-]+\.?)+))?'
+
+
 class Version:
-    major: int = None
-    minor: int = None
-    patch: int = None
-    pre_release: str = None
-    build: str = None
+    def __init__(self, major: int = None, minor: int = None, patch: int = None,
+                 pre_release: str = None, build: str = None):
+        self.major: int = major
+        self.minor: int = minor
+        self.patch: int = patch
+        self.pre_release: str = pre_release
+        self.build: str = build
 
     @staticmethod
     def parse(version_string: str) -> 'Version':
-        pattern = r'(\d+)\.(\d+)\.(\d+)(?:\-((?:[\w\d\-]+\.?)+))?(?:\+((?:[\w\d\-]+\.?)+))?'
-        match = re.fullmatch(pattern, version_string.strip())
+        match = re.fullmatch(SEMVER_PATTERN, version_string.strip())
         if match:
-            Version.major = int(match.group(1))
-            Version.minor = int(match.group(2))
-            Version.patch = int(match.group(3))
-            Version.pre_release = match.group(4)
-            Version.build = match.group(5)
+            return Version(
+                major=int(match.group(1)),
+                minor=int(match.group(2)),
+                patch=int(match.group(3)),
+                pre_release=match.group(4),
+                build=match.group(5),
+            )
+
         else:
             raise ValueError('`version_string` is not valid to Semantic Versioning Specification')
 
-        return Version()
-
-    @staticmethod
-    def core() -> str:
-        return f'{Version.major}.{Version.minor}.{Version.patch}'
+    @property
+    def core(self) -> str:
+        return f'{self.major}.{self.minor}.{self.patch}'
