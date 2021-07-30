535de51cf10eef273c10efce7dbf8acbe82dce62
dl6nm <mail@dl6nm.de>
2021-07-30 15:31:39 +0200

    Add conftest for mocking an fatal error, raised by git describe, when missing a .git directory

---
+++ tests/conftest.py
@@ -0,0 +1,17 @@
+import pytest
+
+
+@pytest.fixture()
+def mock_git_describe_fatal_error():
+    """
+    Mock a fatal error produced by git describe, if the .git directory is missing in repository
+
+    :return: A mocked CompletedProcess object
+    """
+    from subprocess import CompletedProcess
+    return CompletedProcess(
+        args=['git', 'describe'],
+        returncode=128,
+        stdout=b'',
+        stderr=b'fatal: not a git repository (or any of the parent directories): .git\n'
+    )
