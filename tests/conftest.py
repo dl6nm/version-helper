4ca6e001cbc6edb93b4cea3f99bf55fb859b3f18
dl6nm <mail@dl6nm.de>
2021-08-04 11:30:50 +0200

    Add fixtures for (not) installed git binary

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,5 +1,32 @@
 import pytest
 
+from subprocess import CompletedProcess
+
+
+@pytest.fixture
+def git_installed() -> CompletedProcess:
+    """
+    Mock an installed git instance, returning the installation path
+    """
+    return CompletedProcess(
+        args=['git', '--exec-path'],
+        returncode=0,
+        stdout=b'C:/Git',
+        stderr=b'',
+    )
+
+
+@pytest.fixture
+def git_not_installed() -> CompletedProcess:
+    """
+    Mock a not installed git instance, returning an error
+    """
+    return CompletedProcess(
+        args=['git', '--exec-path'],
+        returncode=127,
+        stdout=b'',
+        stderr=b'Der Befehl "git" ist entweder falsch geschrieben oder konnte nicht gefunden werden.',
+    )
 
 
 @pytest.fixture
