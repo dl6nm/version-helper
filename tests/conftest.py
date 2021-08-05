646ca34719abd1e5e5c86ba9008bb88428398797
dl6nm <mail@dl6nm.de>
2021-08-05 16:17:59 +0200

    Change test to mock a git call process with request parameters from the test function

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,44 +2,17 @@ import pytest
 
 from subprocess import CompletedProcess
 
-
-@pytest.fixture
-def git_installed() -> CompletedProcess:
-    """
-    Mock an installed git instance, returning the installation path
-    """
-    return CompletedProcess(
-        args=['git', '--exec-path'],
-        returncode=0,
-        stdout=b'C:/Git',
-        stderr=b'',
-    )
+from version_helper import Git
 
 
 @pytest.fixture
-def git_not_installed() -> CompletedProcess:
-    """
-    Mock a not installed git instance, returning an error
-    """
-    return CompletedProcess(
-        args=['git', '--exec-path'],
-        returncode=127,
-        stdout=b'',
-        stderr=b'Der Befehl "git" ist entweder falsch geschrieben oder konnte nicht gefunden werden.',
-    )
-
-
-@pytest.fixture
-def git_describe_fatal_error():
-    """
-    Mock a fatal error produced by git describe, if the .git directory is missing in repository
-
-    :return: A mocked CompletedProcess object
-    """
-    from subprocess import CompletedProcess
-    return CompletedProcess(
-        args=['git', 'describe'],
-        returncode=128,
-        stdout=b'',
-        stderr=b'fatal: not a git repository (or any of the parent directories): .git\n'
-    )
+def mock_git_call_process(monkeypatch, request) -> CompletedProcess:
+    def fake_call_process(*args, **kwargs):
+        return CompletedProcess(
+            args=request.param.get('args'),
+            returncode=request.param.get('returncode'),
+            stdout=request.param.get('stdout'),
+            stderr=request.param.get('stderr'),
+        )
+    monkeypatch.setattr(Git, '_call_process', fake_call_process)
+    return Git._call_process()
