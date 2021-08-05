a4d8046925de766b1aa3ae06406b6151d0448eb6
dl6nm <mail@dl6nm.de>
2021-08-05 16:18:31 +0200

    Add test for checking if git is installed on the system

--- tests/test_git_tools.py
+++ tests/test_git_tools.py
@@ -3,11 +3,38 @@ import pytest
 from version_helper import Git
 
 
-def test_is_git_available():
-    """
-    Test if git is available on the system
-    """
-    pass
+def test_construction():
+    assert Git()
+
+
+@pytest.mark.parametrize(
+    argnames=['mock_git_call_process', 'expected'],
+    argvalues=[
+        [
+            {
+                'args': ['git', '--exec-path'],
+                'returncode': 0,
+                'stdout': b'C:/Git/asdf',
+                'stderr': b'',
+            },
+            'C:/Git/asdf',
+        ],
+        [
+            {
+                'args': ['git', '--exec-path'],
+                'returncode': 127,
+                'stdout': b'',
+                'stderr': b'Der Befehl "git" ist entweder falsch geschrieben oder\nkonnte nicht gefunden werden.',
+            },
+            None,
+        ],
+    ],
+    indirect=['mock_git_call_process'],
+    ids=['installed', 'not installed']
+)
+def test_is_git_installed(monkeypatch, mock_git_call_process, expected):
+    """Test (mock) if git is installed on the system"""
+    assert Git.exec_path() == expected
 
 
 @pytest.mark.skip(reason='Not implemented yet')
@@ -25,6 +52,7 @@ def test_git_describe_fatal_error(git_describe_fatal_error):
     assert proc.stderr.decode().strip() == 'fatal: not a git repository (or any of the parent directories): .git'
 
 
+@pytest.mark.skip(reason='Not implemented yet')
 @pytest.fixture(
     params=[
         '46467a2',
@@ -33,9 +61,10 @@ def test_git_describe_fatal_error(git_describe_fatal_error):
     ]
 )
 def mock_git_describe():
-    pass
+    assert False
 
 
+@pytest.mark.skip(reason='Not implemented yet')
 @pytest.mark.parametrize(
     argnames=['expected'],
     argvalues=[
@@ -45,6 +74,6 @@ def mock_git_describe():
     ],
 )
 def test_git_describe(mock_git_describe, expected):
-    pass
+    assert False
     # git_describe = Git.describe(dirty=False)
     # assert git_describe == expected
