29522c947cd1b2b849a4976c176d744c9f89164a
dl6nm <mail@dl6nm.de>
2021-08-05 16:19:17 +0200

    Add first implementation of static methods in Git

--- version_helper/git_tools.py
+++ version_helper/git_tools.py
@@ -2,12 +2,20 @@ import subprocess
 
 
 class Git:
-    pass
 
+    @staticmethod
+    def _call_process(*args, **kwargs) -> subprocess.CompletedProcess:
+        print('_call_process')
+        return subprocess.run(*args, **kwargs, capture_output=True)
 
-def get_version():
-    """ Get the current application version from git describe """
-    return subprocess.check_output(['git', 'describe']).strip().decode('utf-8')
+    @staticmethod
+    def exec_path() -> str:
+        """Get the installation path of git or None if not found"""
+        args = ['git', '--exec-path']
+        proc = Git._call_process(args)
+        return proc.stdout.strip().decode('utf-8')
 
-
-__version__ = get_version()
+    @staticmethod
+    def get_version():
+        """ Get the current application version from git describe """
+        return subprocess.check_output(['git', 'describe']).strip().decode('utf-8')
