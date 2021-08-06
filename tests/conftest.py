9edea0ce9c4b9149ed819f7c015df1656fc80ce4
dl6nm <mail@dl6nm.de>
2021-08-06 15:18:01 +0200

    Add test for git describe

--- tests/conftest.py
+++ tests/conftest.py
@@ -7,10 +7,17 @@ from version_helper import Git
 
 
 @pytest.fixture(scope='function')
-def mock_subprocess(monkeypatch, subprocess_exec_path_parameters) -> CompletedProcess:
+def mock_subprocess(monkeypatch, request) -> CompletedProcess:
     """Mock a command line call (subprocess)"""
+    try:
+        # params are directly given
+        params = request.param
+    except AttributeError:
+        # params derived from a parent request
+        params = request._parent_request.param
+
     def fake_subprocess(*args, **kwargs):
-        return subprocess_exec_path_parameters.get("process")
+        return params.get("process")
     monkeypatch.setattr(Git, '_call_process', fake_subprocess)
     return Git._call_process()
 
@@ -56,6 +63,34 @@ def mock_subprocess(monkeypatch, subprocess_exec_path_parameters) -> CompletedPr
     ],
     ids=['installed on win [str]', 'installed on win [bytes]', 'installed on linux', 'not installed'],
 )
-def subprocess_exec_path_parameters(request):
+def git_exec_path_parameters(request, mock_subprocess):
     """Fixture for returning subprocess parameters for `git --exec-path`"""
+    return request.param
+
+
+@pytest.fixture(
+    params=[
+        {
+            'process': CompletedProcess(
+                args=['git', 'describe'],
+                returncode=0,
+                stdout=r'0.0.1-31-gdc27049',
+                stderr=r'',
+            ),
+            'expected': '0.0.1-31-gdc27049',
+        },
+        {
+            'process': CompletedProcess(
+                args=['git', 'describe'],
+                returncode=128,
+                stdout=r'',
+                stderr=r'fatal: not a git repository (or any of the parent directories): .git',
+            ),
+            'expected': None,
+        },
+    ],
+    ids=['success', 'fatal error'],
+)
+def git_describe_parameters(request, mock_subprocess):
+    """Fixture for returning subprocess parameters for `git describe`"""
     return request.param
--- tests/test_git_tools.py
+++ tests/test_git_tools.py
@@ -10,10 +10,10 @@ def test_construction():
     assert Git()
 
 
-def test_call_process(mock_subprocess, subprocess_exec_path_parameters):
-    """Test for calling a process from the Git class"""
+def test_call_process_git_exec_path(git_exec_path_parameters):
+    """Test for calling a process from the Git class (git --exec-path)"""
     proc = Git._call_process()
-    expexted_process: CompletedProcess = subprocess_exec_path_parameters.get('process')
+    expexted_process: CompletedProcess = git_exec_path_parameters.get('process')
 
     assert proc.args == expexted_process.args
     assert proc.returncode == expexted_process.returncode
@@ -21,51 +21,22 @@ def test_call_process(mock_subprocess, subprocess_exec_path_parameters):
     assert proc.stderr == expexted_process.stderr
 
 
-def test_git_exec_path(mock_subprocess, subprocess_exec_path_parameters):
+def test_git_exec_path(git_exec_path_parameters):
     """Test (mock) if git is installed on the system by calling --exec-path"""
-    assert Git.exec_path() == subprocess_exec_path_parameters.get('exec_path')
+    assert Git.exec_path() == git_exec_path_parameters.get('exec_path')
 
 
-@pytest.mark.skip(reason='Not implemented yet')
-@pytest.mark.parametrize(
-    argnames=[],
-    argvalues=[],
-    ids=[],
-)
-def test_git_describe_fatal_error(git_describe_fatal_error):
-    """Check if git describe returns a fatal error on stderr"""
-    proc = git_describe_fatal_error
+def test_call_process_git_describe(git_describe_parameters):
+    """Test for calling a process from the Git class (git --exec-path)"""
+    proc = Git._call_process()
+    expexted_process: CompletedProcess = git_describe_parameters.get('process')
 
-    # Git.describe() is expected to raise an error
-
-    assert proc.args == ['git', 'describe']
-    assert proc.returncode == 128
-    assert proc.stdout.decode().strip() == ''
-    assert proc.stderr.decode().strip() == 'fatal: not a git repository (or any of the parent directories): .git'
+    assert proc.args == expexted_process.args
+    assert proc.returncode == expexted_process.returncode
+    assert proc.stdout == expexted_process.stdout
+    assert proc.stderr == expexted_process.stderr
 
 
-@pytest.mark.skip(reason='Not implemented yet')
-@pytest.fixture(
-    params=[
-        '46467a2',
-        '0.1.2-alpha.0-3-gf0a9091-dirty',
-        '1.2.3-beta.4-3-gf0a9091',
-    ]
-)
-def mock_git_describe():
-    assert False
-
-
-@pytest.mark.skip(reason='Not implemented yet')
-@pytest.mark.parametrize(
-    argnames=['expected'],
-    argvalues=[
-        ['46467a2'],
-        ['0.1.2-alpha.0-3-gf0a9091-dirty'],
-        ['1.2.3-beta.4-3-gf0a9091'],
-    ],
-)
-def test_git_describe(mock_git_describe, expected):
-    assert False
-    # git_describe = Git.describe(dirty=False)
-    # assert git_describe == expected
+def test_git_describe(git_describe_parameters):
+    """Test (mock) if git describe returns the expected values"""
+    assert Git.describe() == git_describe_parameters.get('expected')
