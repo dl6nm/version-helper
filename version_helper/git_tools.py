58b5d85fcdc3f04114dafbc9fd94de6a71b3be3b
dl6nm <mail@dl6nm.de>
2021-08-06 15:18:15 +0200

    Implement git describe

--- version_helper/git_tools.py
+++ version_helper/git_tools.py
@@ -12,19 +12,31 @@ class Git:
     def _call_process(*args, **kwargs) -> subprocess.CompletedProcess:
         return subprocess.run(*args, **kwargs, capture_output=True)
 
-    @staticmethod
-    def exec_path() -> Path:
+    @classmethod
+    def exec_path(cls) -> Path:
         """Get the installation path of git or None if it was not found
 
         :return: Path object to the git executable or None
         """
         args = ['git', '--exec-path']
-        proc = Git._call_process(args)
+        proc = cls._call_process(args)
         if proc.returncode == 0:
             path_str = proc.stdout.strip()
             if type(path_str) is bytes:
                 path_str = path_str.decode('utf-8')
             return Path(path_str)
+
+    @classmethod
+    def describe(cls) -> str or None:
+        if cls.exec_path() is None:
+            return None
+        args = ['git', 'describe']
+        proc = cls._call_process(args)
+        if proc.returncode == 0:
+            path_str = proc.stdout.strip()
+            if type(path_str) is bytes:
+                path_str = path_str.decode('utf-8')
+            return path_str
 
     @staticmethod
     def get_version():
