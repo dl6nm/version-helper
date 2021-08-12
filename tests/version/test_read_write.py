4dca8767757e2bfcf7ae578cd8d799b2d6628b70
dl6nm <mail@dl6nm.de>
2021-08-12 14:39:29 +0200

    Adapt test variable name and value, added TODOs for missing tests

--- tests/version/test_read_write.py
+++ tests/version/test_read_write.py
@@ -4,11 +4,11 @@ from version_helper import Version
 
 
 @pytest.mark.parametrize(
-    argnames=['filename', 'var_name', 'separator', 'full_version'],
+    argnames=['filename', 'variable_name', 'separator', 'full_version'],
     argvalues=[
         ['__version__.py', '__version__', '=', '1.2.3-beta.1+build-987'],
         ['app_version.py', 'APP_VERSION', '=', '0.1.2+build-345'],
-        ['version', None, '=', '2.1.0-rc.1+build-3456'],
+        ['version', None, None, '2.1.0-rc.1+build-3456'],
         ['version.txt', 'version', ':', '1.2.3-beta.1+build-987'],
     ],
     ids=[
@@ -19,8 +19,10 @@ from version_helper import Version
     ]
 )
 class TestReadWriteFile:
+    # TODO: Add test for ValueError(f'Only "=" is allowed as separator for .py files. Could not parse file.')
+    # TODO: Add test for ValueError('None value for separator. Could not parse file.')
+    # TODO: Add test for ValueError('None value for variable_name. Could not parse file.')
 
-    def test_read_file(self, datadir, filename, var_name, separator, full_version):
 
     def test_write_file(self, tmp_path, filename, versions, var_name, separator):
         version: Version = versions
@@ -31,10 +33,11 @@ class TestReadWriteFile:
         # @todo: choose the type of the version to write
 
         version.write_file(
+    def test_read_file(self, datadir, filename, variable_name, separator, full_version):
         file = datadir/filename
         version = Version.read_from_file(
             file=file,
-            variable_name=var_name,
+            variable_name=variable_name,
             separator=separator,
         )
 
