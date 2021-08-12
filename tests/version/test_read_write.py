2b0f9716e38cac08d99f45a13980471842a8c980
dl6nm <mail@dl6nm.de>
2021-08-12 16:38:58 +0200

    Refactor test parameters and move them to a fixture

--- tests/version/test_read_write.py
+++ tests/version/test_read_write.py
@@ -3,13 +3,32 @@ import pytest
 from version_helper import Version
 
 
-@pytest.mark.parametrize(
-    argnames=['filename', 'variable_name', 'separator', 'full_version'],
-    argvalues=[
-        ['__version__.py', '__version__', '=', '1.2.3-beta.1+build-987'],
-        ['app_version.py', 'APP_VERSION', '=', '0.1.2+build-345'],
-        ['version', None, None, '2.1.0-rc.1+build-3456'],
-        ['version.txt', 'version', ':', '1.2.3-beta.1+build-987'],
+@pytest.fixture(
+    params=[
+        {
+            'filename': '__version__.py',
+            'variable_name': '__version__',
+            'separator': '=',
+            'full_version': '1.2.3-beta.1+build-987',
+        },
+        {
+            'filename': 'app_version.py',
+            'variable_name': 'APP_VERSION',
+            'separator': '=',
+            'full_version': '0.1.2+build-345',
+        },
+        {
+            'filename': 'version',
+            'variable_name': None,
+            'separator': None,
+            'full_version': '2.1.0-rc.1+build-3456',
+        },
+        {
+            'filename': 'version.txt',
+            'variable_name': 'version',
+            'separator': ':',
+            'full_version': '1.2.3-beta.1+build-987',
+        },
     ],
     ids=[
         '__version__.py',
@@ -18,6 +37,10 @@ from version_helper import Version
         'version.txt',
     ]
 )
+def read_file_parameters(request):
+    return request.param
+
+
 class TestReadWriteFile:
     # TODO: Add test for ValueError(f'Only "=" is allowed as separator for .py files. Could not parse file.')
     # TODO: Add test for ValueError('None value for separator. Could not parse file.')
@@ -27,13 +50,17 @@ class TestReadWriteFile:
     def test_write_file(self, tmp_path, filename, versions, var_name, separator):
         version: Version = versions
         file = tmp_path/filename
+    def test_read_file(self, datadir, read_file_parameters):
+        filename = read_file_parameters.get('filename')
+        variable_name = read_file_parameters.get('variable_name')
+        separator = read_file_parameters.get('separator')
+        full_version = read_file_parameters.get('full_version')
 
         # @todo: append the versio to the file
         # @todo: overwrite the version in the file
         # @todo: choose the type of the version to write
 
         version.write_file(
-    def test_read_file(self, datadir, filename, variable_name, separator, full_version):
         file = datadir/filename
         version = Version.read_from_file(
             file=file,
